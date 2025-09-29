94e85e4a0ffe1bf1d9504560a662346ed2aebac8
Bethan Linbourn <bcl1999@hotmail.co.uk>
2025-09-29 13:22:28 +0100

    Final touches before Beta Testing

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -28,7 +28,7 @@ public class MainMenu : MonoBehaviour
     public void StartButtonHooverOn()
     {
         startButtonText.color = Color.white;
-        startButton.image.color = new Color(0.24f, 0.55f, 1f);
+        startButton.image.color = new Color(0.13f, 0.02f, 71f);
     }
 
     public void StartButtonHooverOff()
@@ -40,12 +40,12 @@ public class MainMenu : MonoBehaviour
     public void ExitButtonHooverOn()
     {
         quitButtonText.color = Color.white;
-        quitButton.image.color = new Color(0.24f, 0.55f, 1f);
+        quitButton.image.color = new Color(0.13f, 0.02f, 71f);
     }
 
     public void ExitButtonHooverOff()
     {
         quitButtonText.color = Color.black;
-        startButton.image.color = new Color(0.42f, 0.65f, 0.97f);
+        quitButton.image.color = new Color(0.42f, 0.65f, 0.97f);
     }
 }
